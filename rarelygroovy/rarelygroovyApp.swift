d604bc51c3950c422906c4cf6ddad717d30e3354
vendingcompute <edutre1999@gmail.com>
2025-03-18 18:08:55 -0500

    Initial Commit

---
+++ rarelygroovy/rarelygroovyApp.swift
@@ -0,0 +1,17 @@
+//
+//  rarelygroovyApp.swift
+//  rarelygroovy
+//
+//  Created by abs on 3/18/25.
+//
+
+import SwiftUI
+
+@main
+struct rarelygroovyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
