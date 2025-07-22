934e69b915e7751c1fcaad9ddb8176fbcdb3433a
vendingcompute <edutre1999@gmail.com>
2025-07-22 12:11:08 -0500

    added 2 appium tests

--- appium/test/specs/test.e2e.ts
+++ appium/test/specs/test.e2e.ts
@@ -1,9 +1,20 @@
-import { expect, browser, $ } from '@wdio/globals'
+import { expect, $ } from '@wdio/globals'
 
 describe('Rarelygroovy app', () => {
   it('should open home screen', async () => {
     const homeTitle = await $('~calendar') // using accessibility id
     await expect(homeTitle).toBeDisplayed()
+  })
+    it('should load events', async () => {
+    const eventsListedLabel = await $('~eventsListedLabel') // using accessibility id
+    await expect(eventsListedLabel).toBeDisplayed()
+  })
+    it('should navigate to the Artist Directory and load', async () => {
+        const artistDirectoryBtn = await $('~Artist Directory');
+        await artistDirectoryBtn.click();
+
+        const firstBandName = await $('~. . . And The Hero Prevails') // using accessibility id
+        await expect(firstBandName).toBeDisplayed()
   })
 })
 
